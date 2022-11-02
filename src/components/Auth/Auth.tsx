144ca02707f3dc4ea9b869a9403e896f63f12eb0
aswick <aswickjothi27@gmail.com>
2022-11-02 10:22:48 +0530

    sdsd

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React,{useState} from "react";
 import "../Auth/Auth.css";
 
 type Props = {};
 
 const Auth = () => {
+
+    const [email,setEmail] = useState("")
+    const [password,setPassword] = useState("")
+
+    const handleEmail = (e:any) => {
+        const email = e.target.value
+        setEmail(email)
+    }
+
+    const hanldePassword = (e:any) => {
+        const password = e.target.value
+        setPassword(password)
+    }
+
+    const onSubmit = (e:any) => {
+        e.preventDefault();
+        console.log(email,password);
+    }
+
   return (
     <div className="auth__body">
       <div className="auth__container">
@@ -11,15 +30,15 @@ const Auth = () => {
           <div className="auth__banner">
             <img src="https://routemobile.com/wp-content/uploads/2021/06/Login-illustration.svg" />
           </div>
-          <div className="auth__form">
+          <form onSubmit={onSubmit} className="auth__form">
             <img
               src="https://i.pinimg.com/236x/4d/a8/bb/4da8bb993057c69a85b9b6f2775c9df2.jpg"
               alt="profile"
             />
-            <p>Login</p>
+            <h3>Login</h3>
             {/* <input type="text" placeholder="Username" /> */}
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
+            <input type="email" value={email} placeholder="Email" onChange={handleEmail} />
+            <input type="password" value={password} placeholder="Password" onChange={hanldePassword} />
             <div className="auth__checkme ">
               <div className="auth__checkbox">
                 <label>
@@ -30,7 +49,7 @@ const Auth = () => {
                 <label>Forgot Password</label>
               </div>
             </div>
-            <button className="auth__button">Sign In</button>
+            <button type = "submit" className="auth__button">Sign In</button>
 
             <div className="auth__or">
               <hr />
@@ -39,7 +58,7 @@ const Auth = () => {
             </div>
             <button className="auth__button2">Sign In with Google</button>
             <h6>Not registered yet ? Create an account</h6>
-          </div>
+          </form>
         </div>
       </div>
     </div>
