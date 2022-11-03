a8738848b1c6be2b97316de02f07e32d7ea9f36f
aswick <aswickjothi27@gmail.com>
2022-11-03 12:19:53 +0530

    sss

---
+++ src/components/Helpers/Carousel.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Carousel from 'better-react-carousel'
+
+type Props = {}
+
+
+
+const CarouselComp = () => {
+  return (
+    <div style={{top:"17rem",width:"80%",height:"90%",left:"-9rem"}}>
+    <Carousel cols={1} rows={1} gap={10} loop>
+      <Carousel.Item>
+        <img width="100%" src="https://img.freepik.com/premium-photo/modern-korean-girl-shopping-holding-store-bag-talking-mobile-phone-smiling-standing-white-background_1258-102632.jpg?w=4000" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img width="100%" src="https://img.freepik.com/premium-photo/stylish-asian-girl-shopper-wears-sunglasses-holding-shopping-bag-smartphone-going-discounts-stores-standing-white-background_1258-100753.jpg?w=4000" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img width="100%" src="https://img.freepik.com/free-photo/image-stylish-asian-girl-shopper-holding-smartphone-shopping-bag-without-store-logo-wearing-medical-face-mask-from-covid19-white-background_1258-99219.jpg?w=4000&t=st=1667457498~exp=1667458098~hmac=027a1d0e2a0aa7dcd3c3a8eb668d7900157d71aead864761745b6ca5f015697b" />
+      </Carousel.Item>
+    </Carousel>
+    </div>
+  )
+}
+
+export default CarouselComp;
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,14 +1,27 @@
-import React from 'react'
-import Navbar from '../Navbar/Navbar'
+import React from "react";
+import CarouselComp from "../Helpers/Carousel";
+import Navbar from "../Navbar/Navbar";
+import "./Home.css";
 
-type Props = {}
+type Props = {};
 
 const Home = (props: Props) => {
   return (
     <div>
-        <Navbar/>
+      <Navbar />
+      <div className="home__caro">
+      <CarouselComp/>
+      </div>
+      <div className="home__bgclr">
+        {/* <div >
+          <img className="home__img"
+            src="https://img.freepik.com/free-photo/portrait-pretty-happy-girl-talking-mobile-phone_171337-2610.jpg?w=4000&t=st=1667452947~exp=1667453547~hmac=197dffbf839b3a71709b001f84ee4b8dbc2c2385e6777f1f5a6cda2581e1db7c"
+            alt="dd"
+          />
+        </div> */}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
